8058f31f8d36246f63b8f3c5494b7ea26b0ccaee
PrathameyshHacks <t220144@famt.ac.in>
2026-01-11 21:09:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://t220144:se_it_02@cluster0.aabzhxv.mongodb.net/ShreeSort?appName=Cluster0');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Error:", error.message);
